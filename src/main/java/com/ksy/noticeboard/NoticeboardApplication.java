9b36021f98b4152fcb4d0e079962835e30b68089
soyoungkimm <ksoyoung09@gmail.com>
2022-07-14 01:44:50 +0900

    프로젝트 생성

---
+++ src/main/java/com/ksy/noticeboard/NoticeboardApplication.java
@@ -0,0 +1,13 @@
+package com.ksy.noticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksy/noticeboard/NoticeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksy.noticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
