99c140e6dfca4601b71605aee624078970cfce83
soyoungkimm <ksoyoung09@gmail.com>
2022-09-10 00:57:28 +0900

    기능추가: log 추가
    Logback을 이용해서 log를 출력함

--- src/main/java/com/ksy/noticeboard/controller/BoardController.java
+++ src/main/java/com/ksy/noticeboard/controller/BoardController.java
@@ -4,6 +4,8 @@ import com.ksy.noticeboard.dto.Board;
 import com.ksy.noticeboard.service.BoardService;
 import com.ksy.noticeboard.util.Pagination;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,18 +21,22 @@ import java.sql.Timestamp;
 public class BoardController {
 
     private final BoardService boardService;
+    private final Logger LOGGER = LoggerFactory.getLogger(BoardController.class);
 
     @GetMapping("/main")
     public String home() {
+        LOGGER.info("메인 페이지 : home() 메서드 호출");
         return "home";
     }
 
     @GetMapping
     public String list(Model model, @RequestParam(value="p", required = false) Integer p) {
+        LOGGER.info("리스트 페이지 : list() 메서드 호출");
+
         // 페이지네이션
         int curPageNo = (p != null)? p : 1;// 현재 페이지 번호
         int perPageRows = 4; // 한 페이지에 나타날 행의 개수
-        int perPagination = 5; // 한 화면에 나타날 페이지 번호 개수
+        int perPagination =  5; // 한 화면에 나타날 페이지 번호 개수
         int totalRows = boardService.getBoardTotalNum(); // 행의 총 개수
         Pagination pagination = new Pagination(curPageNo, perPageRows, perPagination, totalRows);
         pagination.setFirstRow(pagination.getFirstRow() - 1);
@@ -42,17 +48,21 @@ public class BoardController {
 
     @GetMapping("/{id}")
     public String detail(Model model, @PathVariable int id) {
+        LOGGER.info("상세 페이지 : detail() 메서드 호출");
         model.addAttribute("board", boardService.getBoard(id));
         return "boards/detail";
     }
 
     @GetMapping("/create")
     public String createForm() {
+        LOGGER.info("새 글 작성 페이지 : createForm() 메서드 호출");
         return "boards/create";
     }
 
     @PostMapping
     public String create(@Valid Board board, RedirectAttributes redirectAttributes, BindingResult bindingResult) {
+        LOGGER.info("글 생성 : create() 메서드 호출");
+
         if(bindingResult.hasErrors()) return "boards/create";
 
         int user_id = 1; // 현재 로그인 한 사람 <-- 임시
@@ -67,12 +77,14 @@ public class BoardController {
 
     @GetMapping("/{id}/edit")
     public String updateForm(@PathVariable int id, Model model) {
+        LOGGER.info("글 수정 페이지 : updateForm() 메서드 호출");
         model.addAttribute("board", boardService.getBoard(id));
         return "boards/edit";
     }
 
     @PutMapping("/{id}")
     public String update(@PathVariable int id, @Valid Board board, BindingResult bindingResult) {
+        LOGGER.info("글 수정 : update() 메서드 호출");
         if(bindingResult.hasErrors()) return "boards/edit";
         boardService.updateBoard(board);
         return "redirect:/board/{id}";
@@ -80,6 +92,7 @@ public class BoardController {
 
     @DeleteMapping("/{id}")
     public String deleteMovie(@PathVariable int id) {
+        LOGGER.info("글 삭제 : delete() 메서드 호출");
         boardService.deleteBoard(id);
         return "redirect:/board";
     }
--- src/main/java/com/ksy/noticeboard/controller/ExceptionController.java
+++ src/main/java/com/ksy/noticeboard/controller/ExceptionController.java
@@ -1,27 +1,49 @@
 package com.ksy.noticeboard.controller;
 
 import com.ksy.noticeboard.util.ErrorResponse;
-import org.apache.ibatis.javassist.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.sql.SQLException;
 
 @ControllerAdvice
 public class ExceptionController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ExceptionController.class);
+
     @ExceptionHandler({SQLException.class, DataAccessException.class})
     protected String handleSQLException(Exception e, Model model) {
-        ErrorResponse errorResponse = new ErrorResponse(500, "DB 접속 오류가 발생했습니다.");
+        LOGGER.error("500:sql exception 발생");
+        ErrorResponse errorResponse = new ErrorResponse(500, "DB 로직에 오류가 발생했습니다.");
         model.addAttribute("error", errorResponse);
         return "errors/error";
     }
 
-    @ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler(NoHandlerFoundException.class)
     protected String handleNotFoundException(Exception e, Model model) {
-        ErrorResponse errorResponse = new ErrorResponse(404, "게시글을 찾을 수 없습니다.");
+        LOGGER.error("404:not found error 발생");
+        ErrorResponse errorResponse = new ErrorResponse(404, "페이지를 찾을 수 없습니다.");
+        model.addAttribute("error", errorResponse);
+        return "errors/error";
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    protected String handleNullPointerException(Exception e, Model model) {
+        LOGGER.error("500:null pointer exception 발생");
+        ErrorResponse errorResponse = new ErrorResponse(500, "게시글을 찾을 수 없습니다.");
+        model.addAttribute("error", errorResponse);
+        return "errors/error";
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    protected String handleNumberFormatException(Exception e, Model model) {
+        LOGGER.error("400:number format exception 발생");
+        ErrorResponse errorResponse = new ErrorResponse(400, "숫자가 입력되어야 합니다.");
         model.addAttribute("error", errorResponse);
         return "errors/error";
     }
